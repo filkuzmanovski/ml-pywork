e86143705994f51f0c1b1a98b3f27325e2aa212c
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2016-06-30 17:46:29 -0700

    import errors in ClassifyNB fixed

--- sklearn-1/ClassifyNB.py
+++ sklearn-1/ClassifyNB.py
@@ -1,5 +1,6 @@
+from prep_terrain_data import makeTerrainData
 def classify(features_train, labels_train):
-	from sklearn.naive_bayes import GaussianNB ### sklearn module imported
-	clf = GaussianNB ### classifier created
-	clf.fit(features_train, labels_train) ### Classifier fit on training features and labels
-	pred = clf.predict(features_test) ### Fit Classifier returned
+	from sklearn.naive_bayes import GaussianNB
+	clf = GaussianNB()
+	return clf.fit(features_train, labels_train)
+	pred = clf.predict(features_test)
